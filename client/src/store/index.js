193ed8ab14dbca078e66253f3bcd670f3e35f3f3
Jay Suratwala <81503183+JaySuratwala@users.noreply.github.com>
2024-11-06 07:55:35 +0000

    Initial Project Setup

---
+++ client/src/store/index.js
@@ -0,0 +1,5 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+const rootReducer = combineReducers({});
+const store = createStore(rootReducer, applyMiddleware(thunk));
+export default store;
